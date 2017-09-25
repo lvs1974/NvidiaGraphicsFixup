0ae412bfe6735a28d600883dde61ef3249f6bbd5
lvs1974 <27740976+lvs1974@users.noreply.github.com>
2017-09-25 10:18:01 +0000

    Version 1.2.0: XCode 9 & Lilu 1.2.0 compatibility fixes (Lilu 1.2.0 is required)

--- NvidiaGraphicsFixup/kern_ngfx.cpp
+++ NvidiaGraphicsFixup/kern_ngfx.cpp
@@ -18,7 +18,7 @@
 static const char *kextGraphicsDevicePolicy[] { "/System/Library/Extensions/AppleGraphicsControl.kext/Contents/PlugIns/AppleGraphicsDevicePolicy.kext/Contents/MacOS/AppleGraphicsDevicePolicy" };
 
 static KernelPatcher::KextInfo kextList[] {
-	{ "com.apple.driver.AppleGraphicsDevicePolicy", kextGraphicsDevicePolicy, 1, true, {}, KernelPatcher::KextInfo::Unloaded }
+    { "com.apple.driver.AppleGraphicsDevicePolicy", kextGraphicsDevicePolicy, 1, {true, true}, {}, KernelPatcher::KextInfo::Unloaded }
 };
 
 static size_t kextListSize {1};
@@ -31,7 +31,7 @@ bool NGFX::init() {
 	}, this);
 	
 	if (error != LiluAPI::Error::NoError) {
-		SYSLOG("ngfx @ failed to register onPatcherLoad method %d", error);
+		SYSLOG("ngfx", "failed to register onPatcherLoad method %d", error);
 		return false;
 	}
 	
@@ -46,7 +46,7 @@ void NGFX::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t a
 		for (size_t i = 0; i < kextListSize; i++) {
 			if (kextList[i].loadIndex == index) {
                 if (!(progressState & ProcessingState::GraphicsDevicePolicyPatched) && !strcmp(kextList[i].id, "com.apple.driver.AppleGraphicsDevicePolicy")) {
-                    DBGLOG("ngfx @ found com.apple.driver.AppleGraphicsDevicePolicy");
+                    DBGLOG("ngfx", "found com.apple.driver.AppleGraphicsDevicePolicy");
                     const uint8_t find[]    = {0xBA, 0x05, 0x00, 0x00, 0x00};
                     const uint8_t replace[] = {0xBA, 0x00, 0x00, 0x00, 0x00};
                     KextPatch kext_patch {
@@ -65,12 +65,12 @@ void NGFX::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t a
 }
 
 void NGFX::applyPatches(KernelPatcher &patcher, size_t index, const KextPatch *patches, size_t patchNum) {
-    DBGLOG("ngfx @ applying patches for %zu kext", index);
+    DBGLOG("ngfx", "applying patches for %zu kext", index);
     for (size_t p = 0; p < patchNum; p++) {
         auto &patch = patches[p];
         if (patch.patch.kext->loadIndex == index) {
             if (patcher.compatibleKernel(patch.minKernel, patch.maxKernel)) {
-                DBGLOG("ngfx @ applying %zu patch for %zu kext", p, index);
+                DBGLOG("ngfx", "applying %zu patch for %zu kext", p, index);
                 patcher.applyLookupPatch(&patch.patch);
                 // Do not really care for the errors for now
                 patcher.clearError();
