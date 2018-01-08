ab1802a443e47bce28bbfc00838801c50537b059
lvs1974 <lvs1974@c1f00f59-022f-40d8-a221-166edd2375cd>
2018-01-08 17:18:36 +0000

    Version 1.2.2 - Fix up will be loaded in safe mode (required to fix black screen)

--- trunk/NvidiaGraphicsFixup/kern_start.cpp
+++ trunk/NvidiaGraphicsFixup/kern_start.cpp
@@ -56,7 +56,7 @@ void Configuration::readArguments() {
 PluginConfiguration ADDPR(config) {
 	xStringify(PRODUCT_NAME),
     parseModuleVersion(xStringify(MODULE_VERSION)),
-    LiluAPI::AllowNormal | LiluAPI::AllowInstallerRecovery,
+    LiluAPI::AllowNormal | LiluAPI::AllowInstallerRecovery | LiluAPI::AllowSafeMode,
 	config.bootargOff,
 	arrsize(config.bootargOff),
 	config.bootargDebug,
